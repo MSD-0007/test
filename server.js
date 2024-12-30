972af1bcf411e0a708825d83c5ff97ab1ed82373
NDG <nayandg8@gmail.com>
2024-12-31 00:55:20 +0530

    Replace bcrypt with bcryptjs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Initialize the app
 const app = express();
